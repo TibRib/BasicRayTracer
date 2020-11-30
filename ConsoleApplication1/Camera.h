7f4235859c3c7aec3ee985b6c6544bb25c50459b
TibRib <meta-block@hotmail.fr>
2020-11-30 13:55:14 +0100

    Camera default values

--- ConsoleApplication1/Camera.h
+++ ConsoleApplication1/Camera.h
@@ -16,13 +16,10 @@ private:
 	Vector3 vertical;
 
 public:
-	Camera() {
-		float aspect_ratio = 16.f / 9.f;
-		float viewport_height = 2.f;
+	Camera(float aspect_ratio = 16.f / 9.f, float viewport_height = 2.f, float focal_length =1.0f, Vector3 origin_v=Vector3(0,0,0)) {
 		float viewport_width = aspect_ratio * viewport_height;
-		float focal_length = 1.0;
 
-		origin = Vector3(0, 0, 0);
+		origin = origin_v;
 		horizontal = Vector3(viewport_width, 0.0, 0.0);
 		vertical = Vector3(0.0, viewport_height, 0.0);
 		lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vector3(0, 0, focal_length);
@@ -30,6 +27,10 @@ public:
 
 	Ray get_ray(double u, double v) const {
 		return Ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+	}
+	
+	Vector3 getOrigin() const {
+		return origin;
 	}
 };
 
--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -32,7 +32,7 @@ int main()
 	HDC consoleDC = GetDC(consoleWindow);
 
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 300;
+	const int image_width = 550;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int max_depth = 50;
 	const int samples_per_pixel = 100;
